be4144d9b87d26d883482111508668277d02d4eb
cdamyx <chris@amyx.engineering>
2021-02-11 22:22:06 -0600

    added bmi calc

--- calculator.js
+++ calculator.js
@@ -11,12 +11,26 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-var num1 = Number(req.body.num1);
-var num2 = Number(req.body.num2);
+	var num1 = Number(req.body.num1);
+	var num2 = Number(req.body.num2);
 
-var result = num1 + num2;
+	var result = num1 + num2;
 
 	res.send("The result is " + result);
+});
+
+app.get("/bmicalculator", function(req, res){
+	res.sendFile(__dirname + "/bmiCalculator.html")
+});
+
+app.post("/bmicalculator", function(req, res){
+
+	var weight = parseFloat(req.body.weight);
+	var height = parseFloat(req.body.height);
+
+	var n = weight / height * height;
+
+	res.send("Your BMI is " + n);
 });
 
 app.listen(3000, function(){
